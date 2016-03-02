6172432ce873e85ab2d5352fe8f50901294b5fa1
valmat <ufabiz@gmail.com>
2016-03-03 02:48:21 +0600

    Fixed: examples/Fading/Fading.ino

--- examples/Fading/Fading.ino
+++ examples/Fading/Fading.ino
@@ -15,20 +15,24 @@
 
  This example code is in the public domain.
 
+ ----------------------------------------------
+ Modifed by Valeriy V Dmitriev aka Valmat 
+ As an example to the Pino library
+
  */
 
+#include <Pino.h>
 
-int ledPin = 9;    // LED connected to digital pin 9
 
-void setup() {
-  // nothing happens in setup
-}
+Pino led = 9;   // LED connected to digital pin 9
+
+void setup() {}
 
 void loop() {
   // fade in from min to max in increments of 5 points:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
+    led.write(fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
@@ -36,7 +40,7 @@ void loop() {
   // fade out from max to min in increments of 5 points:
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(ledPin, fadeValue);
+    led.write(fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
